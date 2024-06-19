851f2163bf4ff797bfc08206e4334f492d5c8e45
Lhakpa Tsheri Sherpa <lhakpasherpa090@gmail.com>
2024-06-19 19:56:30 +0545

    Your commit message

--- src/compoents/users/homePageComponents/Scholarships.js
+++ src/compoents/users/homePageComponents/Scholarships.js
@@ -7,18 +7,21 @@ function Scholarships() {
             <h3>The Latest Scholarships</h3>
             <div className="scholarships-container">
                 <div className="scholarship-card">
+                    <img src="/images/users/BIT.jpg" alt="BIT Scholarship" />
                     <h4>BIT Scholarship</h4>
                     <p>Nepal College</p>
                     <p>BIT</p>
                     <p>20%</p>
                 </div>
                 <div className="scholarship-card">
+                    <img src="/images/users/BBA.jpg" alt="BBA Scholarship" />
                     <h4>BBA Scholarship</h4>
                     <p>Nepal College</p>
                     <p>BBA</p>
                     <p>50% </p>
                 </div>
                 <div className="scholarship-card">
+                    <img src="/images/users/BHM.jpg" alt="BHA Scholarship" />
                     <h4>BHA Scholarship</h4>
                     <p>Nepal College</p>
                     <p>BHA</p>
